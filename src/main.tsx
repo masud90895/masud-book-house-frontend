8d9d383f53eeedd0da2ffb13430eb57c560a85cc
masud90895 <masudhossainmbs129@gmail.com>
2023-09-26 04:37:45 +0600

    router added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./Redux/store.ts";
+import router from "./Routes/Router.tsx";
+import { RouterProvider } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+      <Toaster />
+    </Provider>
+  </React.StrictMode>
+);
